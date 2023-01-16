172abafa2cd6bee96ebe7d72c116e4efb65ecadb
jgn <gorinje@gmail.com>
2023-01-16 19:51:05 +0100

    Update openhevc version

--- build_thirdparties.sh
+++ build_thirdparties.sh
@@ -114,14 +114,6 @@ cd $build_path/liba52
 emconfigure $source_path/a52dec-0.7.4/configure  --disable-oss CFLAGS="-fPIC"
 emmake make "${MAKEFLAGS}"
 
-echo "Building openhevc"
-cd $source_path/openHEVC
-git apply ../openHEVC.patch
-mkdir -p $build_path/openHEVC
-cd $build_path/openHEVC
-emcmake cmake $source_path/openHEVC -DCMAKE_C_FLAGS="-fPIC -Wno-incompatible-pointer-types" -DUSE_YASM=OFF -DENABLE_EXECUTABLE=OFF $CMAKE_BUILD_TYPE
-emmake make 
-
 echo "Building ffmpeg"
 mkdir -p $build_path/ffmpeg
 cd $build_path/ffmpeg
@@ -131,6 +123,13 @@ fi
 
 emconfigure $source_path/ffmpeg/configure --target-os=none --arch=x86_32 --enable-cross-compile --disable-x86asm --disable-inline-asm --disable-stripping --disable-programs --disable-doc --disable-runtime-cpudetect --disable-autodetect --disable-pthreads --pkg-config-flags="--static" --nm="$source_path/emsdk/upstream/bin/llvm-nm" --ar=emar --ranlib=emranlib --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --enable-pic --enable-shared $ffmpeg_flags
 emmake make "${MAKEFLAGS}"
+
+echo "Building openhevc"
+cd $source_path/openHEVC
+mkdir -p $build_path/openHEVC
+cd $build_path/openHEVC
+emconfigure $source_path/openHEVC/configure --target-os=none --arch=x86_32 --enable-cross-compile --disable-asm --disable-stripping --disable-programs --nm="$source_path/emsdk/upstream/bin/llvm-nm" --ar=emar --ranlib=emranlib --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --enable-pic $ffmpeg_flags
+emmake make openhevc-static
 
 echo "Building openVVC"
 mkdir -p $build_path/OpenVVC
