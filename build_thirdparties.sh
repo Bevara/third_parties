986c9776a3476484b7b2fee260307f893ba7ff7d
jgn <gorinje@gmail.com>
2023-10-18 12:39:07 +0200

    Add ffmpeg compilation

--- build_thirdparties.sh
+++ build_thirdparties.sh
@@ -137,3 +137,8 @@ cd $build_path/openjpeg
 emcmake cmake $source_path/openjpeg -DCMAKE_C_FLAGS="-fPIC" $CMAKE_BUILD_TYPE
 emmake make "${MAKEFLAGS}"
 
+echo "Building ffmpeg"
+cd $source_path/ffmpeg
+mkdir -p $build_path/ffmpeg
+emconfigure $source_path/ffmpeg/configure --target-os=none --arch=x86_32 --enable-cross-compile --disable-x86asm --disable-inline-asm --disable-stripping --disable-programs --disable-doc --disable-runtime-cpudetect --disable-autodetect --disable-pthreads --pkg-config-flags="--static" --nm="$source_path/emsdk/upstream/bin/llvm-nm" --ar=emar --ranlib=emranlib --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --enable-pic --enable-gpl
+emmake make "${MAKEFLAGS}"
