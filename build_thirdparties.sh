2ad5e82ee9c851992d13933adf48136bcf3f4e5e
jgn <gorinje@gmail.com>
2023-10-18 16:23:06 +0200

    fix ffmpeg compilation

--- build_thirdparties.sh
+++ build_thirdparties.sh
@@ -140,5 +140,6 @@ emmake make
 echo "Building ffmpeg"
 cd $source_path/ffmpeg
 mkdir -p $build_path/ffmpeg
+cd $build_path/ffmpeg
 emconfigure $source_path/ffmpeg/configure --target-os=none --arch=x86_32 --enable-cross-compile --disable-x86asm --disable-inline-asm --disable-stripping --disable-programs --disable-doc --disable-runtime-cpudetect --disable-autodetect --disable-pthreads --pkg-config-flags="--static" --nm="$source_path/emsdk/upstream/bin/llvm-nm" --ar=emar --ranlib=emranlib --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --enable-pic --enable-gpl
 emmake make "${MAKEFLAGS}"
