0c08382835cfff832dc897f4a056580b013c54f0
Jérôme Gorin <gorinje@gmail.com>
2025-05-14 13:47:50 +0200

    Add x265

--- build_thirdparties.sh
+++ build_thirdparties.sh
@@ -142,13 +142,13 @@ cd $build_path/x264
 emconfigure $source_path/x264/configure --enable-static --enable-pic --disable-cli  --disable-asm --disable-thread --host=i686-gnu --prefix="$build_path/out"
 emmake make install-lib-static "${MAKEFLAGS}"
 
-# echo "Building ffmpeg"
-# cd $source_path/ffmpeg
-# git apply ../ffmpeg.patch
-# mkdir -p $build_path/ffmpeg
-# cd $build_path/ffmpeg
-# emconfigure $source_path/ffmpeg/configure --target-os=none --arch=x86_32 --enable-cross-compile --disable-x86asm --disable-inline-asm --disable-stripping --disable-programs --disable-doc --disable-runtime-cpudetect --disable-autodetect --disable-pthreads --pkg-config-flags="--static" --nm="$source_path/emsdk/upstream/bin/llvm-nm" --ar=emar --ranlib=emranlib --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --enable-pic --enable-gpl --enable-libx264
-# emmake make "${MAKEFLAGS}"
+
+echo "Building ffmpeg-x264"
+export EM_PKG_CONFIG_PATH=$build_path/out/lib/pkgconfig
+mkdir -p $build_path/ffmpeg-x264
+cd $build_path/ffmpeg-x264
+emconfigure $source_path/ffmpeg/configure --target-os=none --arch=x86_32 --enable-cross-compile --disable-x86asm --disable-inline-asm --disable-stripping --disable-programs --disable-doc --disable-runtime-cpudetect --disable-autodetect --disable-pthreads --pkg-config-flags="--static" --nm="$source_path/emsdk/upstream/bin/llvm-nm" --ar=emar --ranlib=emranlib --cc=emcc --cxx=em++ --objcc=emcc --dep-cc=emcc --enable-pic --enable-gpl --enable-libx264
+emmake make "${MAKEFLAGS}"
 
 echo "Building ffmpeg-flac"
 mkdir -p $build_path/ffmpeg-flac
@@ -224,4 +224,12 @@ cd $source_path/libmpeg2
 mkdir -p $build_path/libmpeg2
 cd $build_path/libmpeg2
 emconfigure ../configure  --host=generic-unknown-linux-gnu --disable-sdl  --enable-static --with-pic
+emmake make "${MAKEFLAGS}"
+
+
+echo "Building libfaad"
+cd $source_path/libfaad
+mkdir -p $build_path/libfaad
+cd $build_path/libfaad
+emconfigure ../configure  --with-pic --enable-static
 emmake make "${MAKEFLAGS}"
